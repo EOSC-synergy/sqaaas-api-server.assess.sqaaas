f393c22d904b724c49d68b155c04520da0290e96
orviz <orviz@ifca.unican.es>
2022-05-11 18:41:59 +0200

    Add JePL file structure

--- .sqa/script.pasty-cerulean-bongo.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/eosc-synergy/sqaaas-api-server &&
-    bandit -f json --quiet  --confidence-level all low medium high --severity-level all low medium high --recursive .
-)
--- .sqa/script.sleazy-ultramarine-orangutan.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/eosc-synergy/sqaaas-api-server &&
-    find_doc_files.py --file-type README CODE_OF_CONDUCT CONTRIBUTING
-)
