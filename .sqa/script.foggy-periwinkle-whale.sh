579d3ed1e0e878f9342dfeb52af031ba66a6d6c8
orviz <orviz@ifca.unican.es>
2022-06-03 12:48:56 +0200

    Add JePL file structure

---
+++ .sqa/script.foggy-periwinkle-whale.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/eosc-synergy/sqaaas-docs &&
+    git rev-parse --is-inside-work-tree
+)
---
+++ .sqa/script.nerdy-myrtle-buzzard.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/eosc-synergy/sqaaas-docs &&
+    find_doc_files.py --file_type all
+)
