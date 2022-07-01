ac4dedadbebb380518cbae8aaaa0b08c0b82fd72
orviz <orviz@ifca.unican.es>
2022-07-01 11:22:52 +0200

    Add JePL file structure

--- .sqa/script.cheeky-maroon-albatross.sh
+++ .sqa/script.cheeky-maroon-albatross.sh
@@ -1,4 +1,4 @@
 (
-cd github.com/EOSC-synergy/sqaaas-api-server &&
+cd github.com/eosc-synergy/sqaaas-api-server &&
     fuji.py -OID http://hdl.handle.net/10261/157765 --tool_endpoint http://localhost:1071/fuji/api/v1/evaluate -T oai_pmh -E http://digital.csic.es/dspace-oai/request
 )
--- .sqa/script.cozy-rose-cockroach.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/EOSC-synergy/sqaaas-api-server &&
-    licensee detect . --json
-)
